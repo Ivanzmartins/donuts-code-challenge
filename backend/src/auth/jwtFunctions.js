61842d7004d5293c530e3875fe9f431fde69ef7a
Ivanzmartins <ivanzmartins@gmail.com>
2022-12-05 14:11:12 -0300

    Cria pasta de criação de tokens

---
+++ backend/src/auth/jwtFunctions.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'secret';
+
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const createToken = (userWithoutPassword) => {
+  const token = jwt.sign({ data: userWithoutPassword }, secret, jwtConfig);
+  return token;
+};
+
+const validateToken = (authorization) => {
+  try {
+  const payload = jwt.verify(authorization, secret);
+  return payload;
+  } catch (error) {
+    return { isError: error };
+  }
+};
+
+module.exports = {
+  createToken,
+  validateToken,
+};
